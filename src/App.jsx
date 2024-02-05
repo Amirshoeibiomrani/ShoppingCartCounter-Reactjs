dde4bc8177b007b3dd404a216c193a9e45a2d2f1
user.name:"Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2024-02-04 19:09:52 -0600

    config axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom';
+import {Routes,Navigate, Route} from 'react-router-dom';
+import ProductsPage from './pages/ProductsPage';
+import DetailsPage from './pages/DetailsPage';
+import PageNotFound from './pages/404';
+import CheckoutPage from './pages/CheckoutPage';
 
 function App() {
   return (
     <Routes>
-      <Route index element={<Navigate to="/products" replace} />
-      <Route path='/products' />
+      <Route index element={<Navigate to="/products" replace />} />
+      <Route path='/products' element={<ProductsPage />} />
+      <Route path='/products/:id' element={<DetailsPage />} />
+      <Route path='/checkout' element={<CheckoutPage />} />
       
     </Routes>
   )
