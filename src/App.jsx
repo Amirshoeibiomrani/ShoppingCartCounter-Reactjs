096722bcc900b47471205f9d21f47871e914a113
user.name:"Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2024-02-04 19:29:03 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
 import {Routes,Navigate, Route} from 'react-router-dom';
 import ProductsPage from './pages/ProductsPage';
 import DetailsPage from './pages/DetailsPage';
 import PageNotFound from './pages/404';
 import CheckoutPage from './pages/CheckoutPage';
+import ProductsProvider from './context/ProductsContext';
 
 function App() {
   return (
+    <ProductsProvider>
+
     <Routes>
       <Route index element={<Navigate to="/products" replace />} />
       <Route path='/products' element={<ProductsPage />} />
       <Route path='/products/:id' element={<DetailsPage />} />
       <Route path='/checkout' element={<CheckoutPage />} />
+      <Route path='/*' element={<PageNotFound />} />
       
     </Routes>
+    </ProductsProvider>
   )
 }
 
