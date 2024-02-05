05c3a67ab652b53817a8d66167c9a567cd1637b6
user.name:"Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2024-02-04 18:51:38 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
-import "./App.css";
+import React from 'react'
+import {Routes, Route} from 'react-router-dom';
 
-import React from 'react';
-import {Route, Redirect, Switch} from "react-router-dom";
-
-// Components
-import Store from './components/Store';
-import ProductDetails from './components/ProductDetails';
-import Navbar from './components/shared/Navbar';
-import ShopCart from './components/ShopCart';
-
-// Context
-import ProductContextProvider from './context/ProductContextProvider';
-import CartContextProvider from './context/CartContextProvider';
-
-
-const App = () => {
-
+function App() {
   return (
-   <ProductContextProvider>
-    <CartContextProvider>
-      <Navbar />
-    <Switch>
-      <Route path="/products/:id" component={ProductDetails} />
-      <Route path="/products" component={Store} />
-      <Route path="/cart" component={ShopCart} />
-      <Redirect to="/products" />
-    </Switch>
-    </CartContextProvider>
-   </ProductContextProvider>
-  );
-};
+    <Routes>
+      <Route index element={<Navigate to="/products" replace} />
+      <Route path='/products' />
+      
+    </Routes>
+  )
+}
 
-export default App;
+export default App
