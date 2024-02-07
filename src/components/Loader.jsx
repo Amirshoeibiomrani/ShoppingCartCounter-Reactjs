74e2f7d53e0b63deb91a9ee907aa31c8e2302f51
user.name:"Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2024-02-07 03:38:57 -0600

    Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { RotatingLines } from 'react-loader-spinner'
 
 function Loader() {
   return (
-    <div>Loader</div>
+    <div className='w-full h-[1000px] text-center mt-[100px]'> 
+    <RotatingLines width='100px' height="100px" strokeWidth='3' strokeColor='#fe5d42' />
+    </div>
   )
 }
 
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
+import {ImSearch} from "react-icons/im"
+
 import Card from "../components/Card"
+import Loader from "../components/Loader"
 import {useProducts} from "../context/ProductContext"
 
 function ProductsPage() {
   const products = useProducts()
-  console.log(products)
+
+const [search ,setSearch] = useState("")
+ 
+const searchHandler = ()=>{
+  console.log(search)
+}
   return (
+   <>
+    <div className="">
+      <input type="text" placeholder="Search ..." value={search} onChange={e=>setSearch(e.target.value.toLowerCase().trim())} />
+      <button onClick={searchHandler}><ImSearch /></button>
+    </div>
     <div className="flex justify-between">
       <div className="flex flex-wrap justify-between w-full">
-        {!products.length&& <p>Loading...</p>}
+        {!products.length&& <Loader />}
         {products.map((p)=>(
           <Card key={p.id} data={p}/>
         ))}
@@ -15,6 +29,7 @@ function ProductsPage() {
       </div>
       <div>sidebar</div>
     </div>
+    </>
   )
 }
 
