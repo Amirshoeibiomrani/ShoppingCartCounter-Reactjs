492080582121bb1b604647cc4e1be78fbafaec34
user.name:"Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2024-02-08 18:41:43 -0600

    Details

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,22 +5,22 @@ import { shortenText } from "../helper/helper";
 function Card({ data }) {
   const { id, title, image, price } = data;
   return (
-    <div className="w-[270pc] m-[10px] p-5 flex flex-col justify-end items-start bg-[#fff] border rounded-lg border-dashed border-[#e2e2e2]">
+    <div className="w-[270px] m-[10px] p-5 flex flex-col justify-end items-start bg-[#fff]  rounded-[20px] border-dashed border-[#e2e2e2] border-2">
       <img
         src={image}
         alt={title}
-        className="w-[150px]"
+    
         className="w-[230px] h-[230px] p-5 mb-5 bg-[#fff]"
       />
-      <h3 className="text-[#fe5d42]">{shortenText(title)}</h3>
+      <h3 className="text-[#fe5d42] text-[1.1rem]">{shortenText(title)}</h3>
 
-      <p className="text-[#666] font-[0.9rem] font-[600] mt-2.5 mr-0 mb-[30px] ">
+      <p className="text-[#666] text-[0.9rem] font-[600] mt-2.5 mr-0 mb-[30px] ">
         {price} $
       </p>
-      <div className="flex items-center">
+      <div className="flex items-center justify-between w-full">
         <Link
           to={`/products/${id}`}
-          className="cursor-pointer h-[25px] text-#fe5d42 font-[1.5rem]"
+          className="cursor-pointer h-[25px] text-#fe5d42 text-[1.5rem]"
         >
           <TbListDetails />
         </Link>
