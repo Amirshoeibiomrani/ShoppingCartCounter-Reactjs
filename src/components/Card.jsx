764ee669b7c70ce400f1b2aedcb31c45a9044527
user.name:"Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2024-02-14 05:42:10 -0600

    Handle buttons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom";
 import { TbListDetails, TbShoppingBagCheck } from "react-icons/tb";
-import { shortenText } from "../helper/helper";
-import {useCart} from "../context/CartContext"
+import { MdDeleteOutline } from "react-icons/md";
+
+import { productQuantity, shortenText } from "../helper/helper";
+import { useCart } from "../context/CartContext";
 
 function Card({ data }) {
   const { id, title, image, price } = data;
 
   const [state, dispatch] = useCart();
-console.log(state)
+  // console.log(state);
 
-  const clickHandler = () => {
-    dispatch({ type: "ADD_ITEM", payload: data });
+  // quantity= Tedad Mahsul
+  const quantity = productQuantity(state, id);
+  console.log(quantity);
+
+  const clickHandler = (type) => {
+    dispatch({ type, payload: data });
   };
-   
+
   return (
     <div className="w-[270px] m-[10px] p-5 flex flex-col justify-end items-start bg-[#fff]  rounded-[20px] border-dashed border-[#e2e2e2] border-2">
       <img
@@ -33,12 +39,38 @@ console.log(state)
           <TbListDetails />
         </Link>
         <div className="flex items-center">
-          <button
-            onClick={clickHandler}
-            className="bg-[#fe5d42] text-[#fff] border-none text-[1.7rem] h-8 w-8 leading-8 p-[2px] rounded-lg cursor-pointer"
-          >
-            <TbShoppingBagCheck />
-          </button>
+          {quantity == 1 && (
+            <button
+              onClick={() => clickHandler("REMOVE_ITEM")}
+              className="bg-[#fe5d42] text-[#fff] border-none text-[1.7rem] h-8 w-8 leading-8 p-[4px] rounded-lg cursor-pointer"
+            >
+              <MdDeleteOutline />
+            </button>
+          )}
+          {quantity > 1 && (
+            <button
+              onClick={() => clickHandler("DECREASE")}
+              className="bg-[#fe5d42] text-[#fff] border-none text-[1.7rem] h-8 w-8 leading-8 p-[4px] rounded-lg cursor-pointer"
+            >
+              -
+            </button>
+          )}
+          {!!quantity && <span className="w-5 text-center my-2.5 mx-0">{quantity}</span>}
+          {quantity == 0 ? (
+            <button
+              onClick={() => clickHandler("ADD_ITEM")}
+              className="bg-[#fe5d42] text-[#fff] border-none text-[1.7rem] h-8 w-8 leading-8 p-[4px] rounded-lg cursor-pointer"
+            >
+              <TbShoppingBagCheck />
+            </button>
+          ) : (
+            <button
+              onClick={() => clickHandler("INCREASE")}
+              className="bg-[#fe5d42] text-[#fff] border-none text-[1.7rem] h-8 w-8 leading-8 p-[4px] rounded-lg cursor-pointer"
+            >
+              +
+            </button>
+          )}
         </div>
       </div>
     </div>
