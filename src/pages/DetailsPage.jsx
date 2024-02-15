ed2bbc60c3f64216368d4556b4062d27f97230d5
user.name:"Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2024-02-15 04:14:54 -0600

    Style  DetailsPage

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -20,24 +20,26 @@ function DetailsPage() {
       <img className="w-[300px ] px-[15px] py-[25px] bg-[#fff] border-2 border-dashed border-[#fe5d42] rounded-[50px] m-2.5 mr-[50px]" src={productDetails.image} alt={productDetails.title} />
       <div className="w-full border-[#e2e2e2] border-2 border-dashed m-2.5 p-[25px] rounded-[50px] ">
         
-      <h3 className="text-[#fe5d42] text-[1.5rem ] mb-10 ">{productDetails.title}</h3>
-      <p className="text-gray-600 w-[500px] text-[1.1rem] mb-10>{productDetails.description}</p>
-      <p className="mb-2.5">
-        <SiOpenproject />
+      <h3 className="text-[#fe5d42] text-[1.5rem] mb-10 ">{productDetails.title}</h3>
+      <p className="text-gray-600 w-[500px] text-[1.1rem] mb-10">{productDetails.description}</p>
+      <p className = "mb-2.5">
+        <SiOpenproject className="bg-[#fe5d42] mr-2.5"/>
         {productDetails.category}
-      </p>
-      <div className="">
+      </p> 
+      <div className="flex justify-between">
         <span className="price">
-          <IoMdPricetag />
-          {productDetails.price}$
+          <IoMdPricetag className="bg-[#fe5d42] mr-2.5"/>
+          {productDetails.price} $
         </span>
-      </div>
-        <Link to="/products">
-          <FaArrowLeft />
+        
+        <Link to="/products" className="flex items-center bg-[#fe5d42] px-[5px] py-[10px] text-[#fff] rounded-[12px]">
+          <FaArrowLeft className="mr-2.5"/>
           <span>Back to Shop</span>
         </Link>
       </div>
+      </div>
     </div>
+    
   );
 }
 
