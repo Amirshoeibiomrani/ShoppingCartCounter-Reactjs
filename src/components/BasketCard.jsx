4cc4cc07d1cec8cf1fc99566b8fab079a1789826
user.name:"Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2024-02-20 14:27:01 -0600

    Update

--- src/components/BasketCard.jsx
+++ src/components/BasketCard.jsx
@@ -2,7 +2,7 @@ import { shortenText } from "../helper/helper";
 import { MdDeleteOutline } from "react-icons/md";
 function BasketCard({ data, clickHandler }) {
 
-  const { image, title, quantity } = data;
+  const { image, title, quantity } = data
   return (
     <div className="flex items-center justify-between border border-2 border-dashed-[#e2e2e2] rounded-[4px] p-4 mb-5">
       <img className="w-[50px] h-[50px]" src={image} alt={title} />
@@ -13,9 +13,11 @@ function BasketCard({ data, clickHandler }) {
             <MdDeleteOutline />{" "}
           </button>
         )}
+
         {quantity > 1 && (
           <button className="bg-[#fe5d42] text-[#fff] border-none text-[1.3rem] h-[25px] w-[25px] leading-[25px] p-2 rounded-lg cursor-pointer" onClick={() => clickHandler("DECREASE")}>-</button>
-        )}
+        )} 
+        
         <span className="w-5 mx-2 my-0 text-center">{quantity}</span>
         <button className="bg-[#fe5d42] text-[#fff] border-none text-[1.3rem] h-[25px] w-[25px] leading-[25px] p-2 rounded-lg cursor-pointer" onClick={() => clickHandler("INCREASE")}>+</button>
       </div>
