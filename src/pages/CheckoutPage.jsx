25ed0459270c0dbff21544efe2c78a93f9614512
user.name:"Amir Shoeibi Omrani <amirtahaomrany@gmail.com>
2024-02-21 03:33:32 -0600

    Updating Show data

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,32 +1,32 @@
+import { useDispatch, useSelector } from "react-redux";
 import BasketCard from "../components/BasketCard";
 import BasketSideBar from "../components/BasketSideBar";
 // import { useCart } from "../context/CartContext";
 
 function CheckoutPage() {
-  // const [state, dispatch] = useCart();
+ const state = useSelector((store)=>store.cart)
 
-  // const clickHandler = (type, payload) => dispatch({ type, payload });
+  
+  if(!state.itemsCounter){
+    return(
+      <div className="flex justify-between items-start p-2.5 min-h-[1000px]">
+      <p>Empty</p>
 
-  // if(!state.itemsCounter){
-  //   return(
-  //     <div className="flex justify-between items-start p-2.5 min-h-[1000px]">
-  //     <p>Empty</p>
-
-  //     </div>
-  //   )
-  // }
+      </div>
+    )
+  }
   return (
     <div className="flex justify-between items-start p-2.5 min-h-[1000px]">
-      {/* <BasketSideBar state={state} />
+      <BasketSideBar state={state}   />
       <div className="w-full">
         {state.selectedItems.map((product) => (
           <BasketCard
             key={product.id}
-            data={product}
+            data={product}  
             clickHandler={clickHandler}
           />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 }
